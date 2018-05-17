ab3df87ec19f41d8e2e3f7e006521ea7f758200c
JoelCa <jxcata@gmail.com>
2018-05-17 20:56:38 -0300

    inicia p5

---
+++ p5-parte1/ej13.rkt
@@ -0,0 +1,10 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname ej13) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+; Ejercicio 13
+
+(define (pares l) (cond [(empty? l) l]
+                        [(cons? l) (if (even? (first l))
+                                              (cons (first l)
+                                                    (pares (rest l)))
+                                              (pares (rest l)))]))
