8835f2993dcc8f9f51f978c6a4ffa2630983b935
JoelCa <jxcata@gmail.com>
2018-06-23 13:01:14 -0300

    fin p6

--- p6/ej.rkt
+++ p6/ej.rkt
@@ -1,11 +1,11 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-intermediate-reader.ss" "lang")((modname ej) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp")) #f)))
+; Práctica 6
+
 ; Ejercicio 1
 
 ; sumanat : N -> N -> N
-
-
 
 (define (sumanat n m) (cond [(zero? n) m]
                             [else (add1 (sumanat (sub1 n) m))]))
@@ -17,7 +17,7 @@
 (define (multiplicar n m) (cond [(zero? n) 0]
                                 [else (sumanat m (multiplicar (sub1 n) m))]))
 
-; Ejercicio 4
+; Ejercicio 6
 
 ; intervalo-dec : N -> List N
 
@@ -27,18 +27,18 @@
 ; intervalo : N -> List N
 (define (intervalo n) (reverse (intervalo-dec n)))
 
-; Ejercicio 6
+; Ejercicio 8
 
 (define (fibnat n) (cond [(zero? n) 1]
                          [(equal? n 1) 1]
                          [else (+ (fibnat (sub1 (sub1 n))) (fibnat (sub1 n)))]))
 
-; Ejercicio 7
+; Ejercicio 9
 
 (define (list-fibonacci n) (cond [(zero? n) (cons (fibnat 0) empty)]
                                  [else (cons (fibnat n) (list-fibonacci (sub1 n)))]))
 
-; Ejercicio 10
+; Ejercicio 13
 
 (define ALTO 200)
 
@@ -49,10 +49,19 @@
                                               (circulos (sub1 m)))]))
 
 (define (cuadrados m ang) (cond [(zero? m) (empty-scene ANCHO ALTO)]
-                           [else (place-image (rotate ang (rectangle (sqr m) (sqr m) "outline" "blue")) (/ ANCHO 2) (/ ALTO 2)
+                                [else (place-image (rotate ang (rectangle (sqr m) (sqr m) "outline" "blue")) (/ ANCHO 2) (/ ALTO 2)
                                               (cuadrados (sub1 m) (+ 20 ang)))]))
 
+; Ejercicio 14
+(define (cuotas-aux j total n i) (cond [(zero? j) '()]
+                                       [else (cons (+ (/ total n)
+                                                      (* (/ total n)
+                                                         (* (/ i 1200) j)))
+                                                   (cuotas-aux (sub1 j) total n i))]))
+(check-expect (cuotas 30000 3 12)
+              (list 10100 10200 10300))
 
+(define (cuotas total n i) (reverse (cuotas-aux n total n i)))
 
 
 
